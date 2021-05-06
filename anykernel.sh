fc5a9d7566c42fa108c1b9ee60d542966905659e
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-05-06 11:31:03 +0000

    anykernel.sh: Add daisy and sakura to the kernel name line

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sleepy kernel by Lacia-chan
+kernel.string=Sleepy kernel for Daisy and Sakura
 do.devicecheck=1
 do.modules=0
 do.systemless=1
